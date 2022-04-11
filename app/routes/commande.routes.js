a90acdf0a85b193cd43e53f20c489ead06cb47dc
Tsiky-0512 <tsiky.rasolofomanana@gmail.com>
2022-04-12 01:56:16 +0300

    commande update

--- app/routes/commande.routes.js
+++ app/routes/commande.routes.js
@@ -4,7 +4,7 @@ const { authJwt } = require("../middleware/index.js");
 module.exports = function (app) {
     app.post("/commande",[authJwt.verifyToken,authJwt.isActivate],controller.save);
     app.post("/commande/multiple",[authJwt.verifyToken,authJwt.isActivate],controller.saveMultiple);
-    app.get("/commande",[authJwt.verifyToken,authJwt.isActivate],controller.find);
+    app.post("/commande/list",[authJwt.verifyToken,authJwt.isActivate],controller.find);
     app.put("/commande",[authJwt.verifyToken,authJwt.isActivate],controller.update);
     app.delete("/commande",[authJwt.verifyToken,authJwt.isActivate],controller.delete);
 }
