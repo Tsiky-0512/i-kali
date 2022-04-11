ba715e66c7618939597f029f0e9237aa221d654d
Tsiky-0512 <tsiky.rasolofomanana@gmail.com>
2022-04-11 23:28:22 +0300

    Update

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.use(express.static(__dirname + '/dist/i-kali'));
 
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/i-kali/index.html'));
+  res.sendFile(path.join(__dirname+'dist/i-kali/index.html'));
 });
 
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 80); 
