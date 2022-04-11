3ebaee988576df755b16d9b26505443c547d308d
Tsiky-0512 <tsiky.rasolofomanana@gmail.com>
2022-04-11 21:49:35 +0300

    Update auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,11 +1,9 @@
-// const controller = require("../controllers/auth.controller.js");
-// const { authJwt } = require("../middleware/index.js");
+const controller = require("../controllers/auth.controller.js");
+// const middleware= require("../middleware/index.js");
 
 module.exports = function (app) {
-    // app.post("/auth/user/inscription",controller.inscription);
-    // app.post("/auth/user/check",controller.verifyEmail);
-    // app.post("/auth/user/login",controller.login);
-    app.get("auth/user/signout",(req,res)=>{
-        res.send("hello hello")
-    });
+    app.post("/auth/user/inscription",controller.inscription);
+    app.post("/auth/user/check",controller.verifyEmail);
+    app.post("/auth/user/login",controller.login);
+    // app.get("auth/user/signout",[authJwt.verifyToken],controller.signout);
 }
