3c37cc40e8411f85841602cd1f477ae60af66c3f
Tsiky-0512 <tsiky.rasolofomanana@gmail.com>
2022-04-12 00:40:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get('/',(req,res)=>{
 
 //routes
 require("./app/routes/auth.routes.js")(app); 
-// require("./app/routes/user.routes.js")(app); 
-// require("./app/routes/plat.routes.js")(app); 
-// require("./app/routes/commande.routes.js")(app); 
+require("./app/routes/user.routes.js")(app); 
+require("./app/routes/plat.routes.js")(app); 
+require("./app/routes/commande.routes.js")(app); 
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
