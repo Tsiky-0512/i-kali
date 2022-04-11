821cdfc2da8d284579315925d942c24815ca0ba4
Tsiky-0512 <tsiky.rasolofomanana@gmail.com>
2022-04-11 23:51:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/i-kali'));
 
 
-app.get('/', function(req,res) {
+app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/i-kali/index.html'));
 });
 
